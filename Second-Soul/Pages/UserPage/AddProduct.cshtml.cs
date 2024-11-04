ecb7f34837c3e522f3ef8a9847b18d80f4c2b147
minhghhhh <minhpthse172948@fpt.edu.vn>
2024-11-04 15:05:24 +0700

    đ

--- Second-Soul/Pages/UserPage/AddProduct.cshtml.cs
+++ Second-Soul/Pages/UserPage/AddProduct.cshtml.cs
@@ -174,4 +174,19 @@ namespace Second_Soul.Pages.UserPage
 			return RedirectToPage("/Index");
 		}
 	}
+
+	public static class TempDataExtensions
+	{
+		public static void Set<T>(this ITempDataDictionary tempData, string key, T value) where T : class
+		{
+			tempData[key] = JsonConvert.SerializeObject(value);
+		}
+
+		public static T? Get<T>(this ITempDataDictionary tempData, string key) where T : class
+		{
+			object o;
+			tempData.TryGetValue(key, out o);
+			return o == null ? null : JsonConvert.DeserializeObject<T>((string)o);
+		}
+	}
 }
